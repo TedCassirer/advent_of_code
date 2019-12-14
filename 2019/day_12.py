c2c1c3bee4be48e24e9269cf5265cd6d7871000e
Ted Cassirer <ted.cassirer@gmail.com>
2019-12-14 22:13:10 +0700

    Day 12 part 12 solved, but too slow?

--- 2019/day_12.py
+++ 2019/day_12.py
@@ -38,7 +38,18 @@ class Moon:
     
     def energy(self):
         return self.__kinetic_energy() * self.__potential_energy()
+    
+    def __hash__(self):
+        return hash((self.x, self.y, self.z, self.vx, self.vy, self.vz))
+
+    def x_hash(self):
+        return hash((self.x, self.vx))
         
+    def y_hash(self):
+        return hash((self.y, self.vy))
+    
+    def z_hash(self):
+        return hash((self.z, self.vz))
 
 def get_moons():
     regex = re.compile(r'=(-?\d+)')
@@ -57,10 +68,51 @@ def part1():
         for moon in moons:
             moon.tick()
     return sum(moon.energy() for moon in moons)
-    
+
 def part2():
-    pass
+    periods = []
+    for hash_fun in [Moon.x_hash, Moon.y_hash, Moon.z_hash]:
+        moons = get_moons()
+        states = {}
+        for tick in range(10000000000):
+            for m1 in moons:
+                for m2 in moons:
+                    m1.apply_gravity_from(m2)
+            state = tuple(hash_fun(m) for m in moons)
+            if state in states:
+                periods.append((tick, tick - states[state]))
+                break
+            states[state] = tick
+            for moon in moons:
+                moon.tick()
+    
+    print(periods)
+    x_period_data, y_period_data, z_period_data = periods
+
+    x_tick, x_period = x_period_data
+    y_tick, y_period = y_period_data
+    z_tick, z_period = z_period_data
+
+    search_to = 100
+    while True:
+        print(search_to)
+        x_ticks = {x for x in range(x_tick, search_to, x_period)}
+        x_tick += x_period * len(x_ticks)
+
+        y_ticks = {y for y in range(y_tick, search_to, y_period)}
+        y_tick += y_period * len(y_ticks)
+
+        z_ticks = {z for z in range(z_tick, search_to, z_period)}
+        z_tick += z_period * len(z_ticks)
+
+        intersection = set.intersection(x_ticks, y_ticks, z_ticks)
+        print(intersection)
+        if intersection:
+            return min(intersection)
+
+        search_to += search_to
+
 
 if __name__ == '__main__':
-    print('Part 1:', part1())
+    # print('Part 1:', part1())
     print('Part 2:', part2())

05f98faebcd5ebdf279e252393f80270e209e550
Ted Cassirer <ted.cassirer@gmail.com>
2020-11-19 14:22:13 +0100

    Fixed day 12 part 2

--- 2019/day_12.py
+++ 2019/day_12.py
@@ -1,5 +1,6 @@
 
 import re
+from math import gcd
 class Moon:
     def __init__(self, x, y, z):
         self.x = x
@@ -80,39 +81,18 @@ def part2():
                     m1.apply_gravity_from(m2)
             state = tuple(hash_fun(m) for m in moons)
             if state in states:
-                periods.append((tick, tick - states[state]))
+                periods.append(tick - states[state])
                 break
             states[state] = tick
             for moon in moons:
                 moon.tick()
     
-    print(periods)
-    x_period_data, y_period_data, z_period_data = periods
-
-    x_tick, x_period = x_period_data
-    y_tick, y_period = y_period_data
-    z_tick, z_period = z_period_data
-
-    search_to = 10**12
-    while True:
-        print(search_to)
-        x_ticks = {x for x in range(x_tick, search_to, x_period)}
-        x_tick += x_period * len(x_ticks)
-
-        y_ticks = {y for y in range(y_tick, search_to, y_period)}
-        y_tick += y_period * len(y_ticks)
-
-        z_ticks = {z for z in range(z_tick, search_to, z_period)}
-        z_tick += z_period * len(z_ticks)
-
-        intersection = set.intersection(x_ticks, y_ticks, z_ticks)
-        print(intersection)
-        if intersection:
-            return min(intersection)
-
-        search_to += 10**12
+    x_period, y_period, z_period = periods
+    gcf = gcd(x_period, gcd(y_period, z_period))
+    return (x_period * y_period * z_period) // gcf**3
+    
 
 
 if __name__ == '__main__':
-    # print('Part 1:', part1())
+    print('Part 1:', part1())
     print('Part 2:', part2())

b916615d7f69289f06b0f9104090f1a4ba1597c8
Ted Cassirer <ted.cassirer@gmail.com>
2020-11-27 19:16:58 +0100

    Fast A* solution for part 1 day 18 2019

--- 2019/day_18.py
+++ 2019/day_18.py
@@ -1,6 +1,7 @@
 from functools import lru_cache
-from collections import deque
 from utils import timeIt
+import heapq
+
 
 def getMaze():
     maze = []
@@ -8,6 +9,7 @@ def getMaze():
         for line in file:
             maze.append(line.strip())
         return maze
+
 
 def getKeyLocations(maze):
     keys = {}
@@ -17,56 +19,92 @@ def getKeyLocations(maze):
                 keys[c] = (y, x)
     return keys
 
+
 maze = getMaze()
 keyLocations = getKeyLocations(maze)
+allKeys = frozenset(keyLocations.keys()) - {'@'}
+
+
+def manhattanDistance(p1, p2):
+    return abs(p1[0]-p2[0]) + abs(p1[1]-p2[1])
+
+
+def getNeighbours(p1):
+    for p in ((p1[0]+1, p1[1]), (p1[0]-1, p1[1]), (p1[0], p1[1]+1), (p1[0], p1[1]-1)):
+        if maze[p[0]][p[1]] != '#':
+            yield p
+
 
 @lru_cache(None)
-def getPath(key1, key2):
+def getDist(key1, key2):
+    p2 = keyLocations[key2]
     visited = set()
-    toVisit = deque([(keyLocations[key1], frozenset(), frozenset(), 0)])
+    toVisit = [(0, keyLocations[key1], frozenset(), frozenset(), 0)]
     while toVisit:
-        curr, freeKeys, requiredKeys, steps = toVisit.pop()
-        tile = maze[curr[0]][curr[1]]
-        if curr in visited or tile == '#':
-            continue
-        if tile == key2:
-            return (steps, freeKeys|{key2} , requiredKeys - freeKeys)
-        if tile.isalpha():
-            if tile.isupper():
-                requiredKeys |= {tile.lower()}
-            else:
-                freeKeys |= {tile}
-        for nextStep in ((curr[0]+1, curr[1]), (curr[0]-1, curr[1]), (curr[0], curr[1]+1), (curr[0], curr[1]-1)):
-            toVisit.append((nextStep, freeKeys, requiredKeys, steps+1))
-        visited.add(curr)
+        _, curr, freeKeys, requiredKeys, steps = heapq.heappop(toVisit)
+        for nextStep in getNeighbours(curr):
+            if nextStep in visited:
+                continue
+            visited.add(nextStep)
 
-seen = dict()
-def simpleSearch(current, owned, toFind, currentSteps=0, shortest=1<<32):
-    fp = (current, owned)
-    if fp in seen and seen[fp] <= currentSteps:
-        return shortest
-    seen[fp] = currentSteps
+            if nextStep == p2:
+                return (steps+1, freeKeys | {key2}, requiredKeys - freeKeys)
 
-    if currentSteps >= shortest:
-        return shortest
-    if not toFind:
-        print(currentSteps)
-        return currentSteps
-    for path, k2 in sorted((getPath(current, k2), k2) for k2 in toFind):
-        steps, freeKeys, requiredKeys = path 
-        keys = owned|freeKeys
-        if keys.issuperset(requiredKeys):
-            tmp = simpleSearch(k2, keys, toFind-freeKeys, currentSteps+steps, shortest)
-            shortest = min(shortest, tmp)
-    return shortest
+            tile = maze[nextStep[0]][nextStep[1]]
+            if tile.isalpha():
+                if tile.isupper():
+                    requiredKeys |= {tile.lower()}
+                else:
+                    freeKeys |= {tile}
+            heapq.heappush(toVisit, ((manhattanDistance(nextStep, p2), nextStep, freeKeys, requiredKeys, steps+1)))
+    raise Exception("Uh, this shouldn't happen")
 
-    
+
+def estimateCost(k1, remaining):
+    if not remaining:
+        return 0
+    pos = keyLocations[k1]
+    if len(remaining) == 1:
+        return manhattanDistance(pos, keyLocations[next(iter(remaining))])
+    left, right = min(map(keyLocations.get, remaining)), max(map(keyLocations.get, remaining))
+    firstRoute = min(manhattanDistance(pos, left), manhattanDistance(pos, right))
+    secondRoute = manhattanDistance(left, right)
+    return firstRoute + secondRoute
+
+
+def aStar(maze, start):
+    stuff = [(0, 0, start, frozenset(), allKeys)]
+    seen = dict()
+    while stuff:
+        _, steps, k1, keys, notVisited = heapq.heappop(stuff)
+        for k2 in notVisited:
+            stepsToKey, newKeys, requiredKeys = getDist(k1, k2)
+            if not keys.issuperset(requiredKeys):
+                continue
+            currentCost = steps + stepsToKey
+
+            toVisit = notVisited - newKeys
+            if not toVisit:
+                return currentCost
+
+            fp = (k2, toVisit)
+            if fp in seen and seen[fp] <= currentCost:
+                continue
+            seen[fp] = currentCost
+
+            newKeys |= keys
+            estimatedCost = estimateCost(k2, newKeys)
+            heapq.heappush(stuff, (currentCost + estimatedCost, currentCost, k2, newKeys, toVisit))
+
+
 @timeIt
 def part1():
-    return simpleSearch('@', frozenset('@'), frozenset(keyLocations.keys()) - {'@'}, 0)
+    return aStar(maze, '@')
+
 
 def part2():
     pass
+
 
 if __name__ == '__main__':
     print('Part 1:', part1())
